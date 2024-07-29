af31f4160fbbebcc97c0cb045f4d5df02f1b1cc9
Harshit-Sandilya <2021ucp1382@mnit.ac.in>
2024-07-29 16:52:00 +0530

    Data

---
+++ data/combine.py
@@ -0,0 +1,75 @@
+import pandas as pd
+import glob
+
+file_list = glob.glob("./csv/*.csv")
+
+
+def append_files(file_list):
+    df = pd.read_csv(f"./final/{file_list[0]}.csv")
+    for file in file_list[1:]:
+        df = pd.concat([df, pd.read_csv(f"./final/{file}.csv")], ignore_index=True)
+    df.drop_duplicates(subset=["sequence", "structure"], inplace=True)
+    df.reset_index(drop=True, inplace=True)
+    df.drop_duplicates(subset=["sequence"], inplace=True)
+    df.reset_index(drop=True, inplace=True)
+    return df
+
+
+for file in file_list:
+    df = pd.read_csv(file)
+    print("Original ({}): ".format(file), df.shape[0])
+    df.drop_duplicates(subset=["sequence", "structure"], inplace=True)
+    df.drop_duplicates(subset=["sequence"], inplace=True)
+    df.reset_index(drop=True, inplace=True)
+    print("Filtered ({}): ".format(file), df.shape[0])
+    df.to_csv(file, index=False)
+
+split_list = ["CRW", "RFAM", "RNAstrand", "PDB", "bpRNA"]
+
+for file in split_list:
+    df = pd.read_csv(f"./csv/{file}.csv")
+    print("Original ({}): ".format(file), df.shape[0])
+    train_df = df.sample(frac=0.75, random_state=42)
+    test_df = df.drop(train_df.index)
+    train_df.reset_index(drop=True, inplace=True)
+    test_df.reset_index(drop=True, inplace=True)
+    print("Train ({}): ".format(file), train_df.shape[0])
+    print("Test ({}): ".format(file), test_df.shape[0])
+    train_df.to_csv(f"./final/{file}-train.csv", index=False)
+    test_df.to_csv(f"./final/{file}-test.csv", index=False)
+
+remaining_list = ["TR0", "TR1", "VL0", "VL1", "TS0", "TS1"]
+
+for file in remaining_list:
+    df = pd.read_csv(f"./csv/{file}.csv")
+    df.to_csv(f"./final/{file}.csv", index=False)
+
+train_list = [
+    "CRW-train",
+    "RFAM-train",
+    "RNAstrand-train",
+    "PDB-train",
+    "bpRNA-train",
+    "TR0",
+    "TR1",
+    "VL0",
+    "VL1",
+]
+
+test_list = [
+    "CRW-test",
+    "RFAM-test",
+    "RNAstrand-test",
+    "PDB-test",
+    "bpRNA-test",
+    "TS0",
+    "TS1",
+]
+
+combined_df = append_files(train_list)
+combined_df.to_csv("./final/train.csv", index=False)
+print("Train: ", combined_df.shape[0])
+
+combined_df = append_files(test_list)
+combined_df.to_csv("./final/test.csv", index=False)
+print("Test: ", combined_df.shape[0])
---
+++ data/preprocess.py
@@ -0,0 +1,156 @@
+from Bio import SeqIO
+from tqdm import tqdm
+import pandas as pd
+import glob
+import csv
+import re
+
+
+def generate_dot_bracket(sequence, base_pairs):
+    dot_bracket = ["."] * len(sequence)
+    for i, j in base_pairs:
+        dot_bracket[i - 1] = "("
+        dot_bracket[j - 1] = ")"
+
+    return "".join(dot_bracket)
+
+
+def process_file(file):
+    with open(f"{file}.dp") as f:
+        lines = f.readlines()
+    lines = [line.strip() for line in lines]
+    lines = [
+        line
+        for line in lines
+        if re.search(r"# file", line)
+        or re.search(r"^[guac][guac]+[guac]$", line)
+        or re.search(r"^[().][().]+[().]$", line)
+    ]
+    seq = []
+    str = []
+    Sequences = []
+    Structures = []
+    Files = []
+    for line in tqdm(lines):
+        if re.search(r"# file", line):
+            Sequences.append("".join(seq))
+            Structures.append("".join(str))
+            Files.append(line)
+            seq = []
+            str = []
+        if re.search(r"^[guac][guac]+[guac]$", line):
+            seq.append(line)
+        elif re.search(r"^[().][().]+[().]$", line):
+            str.append(line)
+    Sequences.append("".join(seq))
+    Structures.append("".join(str))
+
+    Sequences = Sequences[1:]
+    Structures = Structures[1:]
+    lengths = [len(sequence) for sequence in Sequences]
+    df = pd.DataFrame(
+        {
+            "RNA_id": Files,
+            "sequence": Sequences,
+            "structure": Structures,
+            "length": lengths,
+            "source": file,
+        }
+    )
+    for i in range(len(df)):
+        df.at[i, "RNA_id"] = df.at[i, "RNA_id"].split(" ")[-1]
+    df = df.drop(df[df["sequence"].str.len() == 0].index)
+    df = df.drop(df[df["structure"].str.len() == 0].index)
+    df = df.drop(df[df["sequence"].str.len() != df["structure"].str.len()].index)
+    df = df.reset_index(drop=True)
+    df.to_csv(f"./csv/{file}.csv", index=False)
+
+
+def process_single(folder, files):
+    mismatch = 0
+    file_list = glob.glob(f"./{folder}/*.{files}")
+    print("Total files: ", len(file_list))
+    with open(f"./csv/{folder}.csv", "w") as f:
+        writer = csv.writer(f)
+        writer.writerow(["RNA_id", "sequence", "structure", "length", "source"])
+        for file in tqdm(file_list):
+            with open(file, "r") as f2:
+                lines = f2.readlines()
+                id = lines[0].strip().split(" ")[1]
+                length = int(re.search(r"\d+", lines[1]).group())
+                sequence = lines[3].strip()
+                structure = lines[4].strip()
+                if (
+                    len(sequence) != len(structure)
+                    or len(sequence) != length
+                    or len(structure) != length
+                ):
+                    mismatch += 1
+                else:
+                    writer.writerow([id, sequence, structure, length, folder])
+    print("Mismatch: ", mismatch)
+
+
+def process_split(folder, file1, file2):
+    mismatch = 0
+    filelist1 = glob.glob(f"./{folder}/*.{file1}")
+    print("Total files: ", len(filelist1))
+    with open(f"./csv/{folder}.csv", "w") as f:
+        writer = csv.writer(f)
+        writer.writerow(["RNA_id", "sequence", "structure", "length", "source"])
+        for file in tqdm(filelist1):
+            with open(file, "r") as f1:
+                records = SeqIO.parse(f1, "fasta")
+                for record in records:
+                    id = record.id
+                    sequence = str(record.seq)
+                    base_pairs = []
+                    with open(
+                        file.replace(file1, file2).replace("fasta", "bps"), "r"
+                    ) as f2:
+                        lines = f2.readlines()
+                        for line in lines[2:]:
+                            line = line.strip().split("\t\t")
+                            base_pairs.append((int(line[0]), int(line[1])))
+                    try:
+                        structure = generate_dot_bracket(sequence, base_pairs)
+                        writer.writerow(
+                            [id, sequence, structure, len(sequence), folder]
+                        )
+                    except:
+                        mismatch += 1
+    print("Mismatch: ", mismatch)
+
+
+process_file("RNAstrand")
+process_split("TR0", "fasta", "bps")
+process_split("TS0", "fasta", "bps")
+process_split("VL0", "fasta", "bps")
+process_split("TR1", "fasta", "bps")
+process_split("TS1", "fasta", "bps")
+process_split("VL1", "fasta", "bps")
+process_single("bpRNA", "dbn")
+
+df = pd.read_csv("./csv/bpRNA.csv")
+df = df[["RFAM" in id for id in df["RNA_id"]]]
+df.reset_index(drop=True, inplace=True)
+df.drop_duplicates(subset=["sequence", "structure"], inplace=True)
+df.reset_index(drop=True, inplace=True)
+print("Filtered (RFAM): ", df.shape[0])
+df.to_csv("./csv/RFAM.csv", index=False)
+
+df = pd.read_csv("./csv/bpRNA.csv")
+df = df[["CRW" in id for id in df["RNA_id"]]]
+df.reset_index(drop=True, inplace=True)
+df.drop_duplicates(subset=["sequence", "structure"], inplace=True)
+df.reset_index(drop=True, inplace=True)
+print("Filtered (CRW): ", df.shape[0])
+df.to_csv("./csv/CRW.csv", index=False)
+
+df = pd.read_csv("./csv/bpRNA.csv")
+df = df[["PDB" in id for id in df["RNA_id"]]]
+df.reset_index(drop=True, inplace=True)
+df.drop_duplicates(subset=["sequence", "structure"], inplace=True)
+df.reset_index(drop=True, inplace=True)
+print("Filtered (PDB): ", df.shape[0])
+df.to_csv("./csv/PDB.csv", index=False)
